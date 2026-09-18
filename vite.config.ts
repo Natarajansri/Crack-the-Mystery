e46058311af260cb5ed1efe0f8faa672ce9750a1
Natarajansri <natarajansri3@gmail.com>
2026-09-18 23:52:34 +0530

    Fix Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- HEAD
-
   base: '/',
- 92f0d6f (feat: complete Crack The Mystery web app with Netlify deployment config)
 })
